f50cbaaed9c9a68e07217844d419ca62de71966c
unknown <djek.uait1998@gmail.com>
2025-07-03 14:49:09 +0300

    first commit

--- source/js/modules/init-hack-slider.js
+++ source/js/modules/init-hack-slider.js
@@ -11,6 +11,24 @@ const initHackSlider = () => {
         grabCursor: true,
         speed: 1000,
         loop: true,
+        resistance: true, // Добавляет "упругость" при перетаскивании
+    resistanceRatio: 0.6, // Уровень сопротивления
+    followFinger: false, // Отключает следование за пальцем (для плавности)
+    touchReleaseOnEdges: true, // Позволяет "отпускать" слайды у краев
+    initialSlide: 1,
+    // Параметры для плавности
+    easing: 'cubic-bezier(0.25, 0.1, 0.25, 1)', // Функция плавности
+    grabCursor: true, // Меняет курсор при наведении
+    
+    // Оптимизации
+    preloadImages: false,
+    lazy: {
+      loadPrevNext: true,
+      loadPrevNextAmount: 2
+    },
+    watchSlidesProgress: true,
+    observer: true,
+    observeParents: true,
         navigation: {
         nextEl: '.swiper-button-next.hack__next',
         prevEl: '.swiper-button-prev.hack__prev',
--- source/js/modules/init-rev-slider.js
+++ source/js/modules/init-rev-slider.js
@@ -10,6 +10,24 @@ const initRevSlider = () => {
         spaceBetween: 16,
         speed: 1000,
         loop: true,
+        resistance: true, // Добавляет "упругость" при перетаскивании
+    resistanceRatio: 0.6, // Уровень сопротивления
+    followFinger: false, // Отключает следование за пальцем (для плавности)
+    touchReleaseOnEdges: true, // Позволяет "отпускать" слайды у краев
+    initialSlide: 1,
+    // Параметры для плавности
+    easing: 'cubic-bezier(0.25, 0.1, 0.25, 1)', // Функция плавности
+    grabCursor: true, // Меняет курсор при наведении
+    
+    // Оптимизации
+    preloadImages: false,
+    lazy: {
+      loadPrevNext: true,
+      loadPrevNextAmount: 2
+    },
+    watchSlidesProgress: true,
+    observer: true,
+    observeParents: true,
         navigation: {
         nextEl: '.swiper-button-next.reviews__next',
         prevEl: '.swiper-button-prev.reviews__prev',
